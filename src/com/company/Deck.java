5489460c6d6fe313bfdfcec041d4251590770582
BT_1N3_24 <achen7295@bths.edu>
2018-12-10 11:42:45 -0500

    Card

---
+++ src/com/company/Deck.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Card> unDealt;
+    private ArrayList<Card> Dealt;
+
+    public Deck(int rank, String suit, int pointValue){
+
+    }
+
+    public boolean isEmpty(){
+        if(Dealt.size() == 0){
+        }
+    }
+}
